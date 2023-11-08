459f3c0c888f7d7c6212d602a63e47408a2bb2dc
LaiLeeHan <allen@profetai.com>
2023-11-09 00:50:17 +0800

    Chore: delete useless code

--- main.go
+++ main.go
@@ -54,31 +54,4 @@ Exit:
 			break Exit
 		}
 	}
-
-	// Read
-	// var product Product
-	// db.First(&product, 1) // find product with integer primary key
-	// db.First(&product, "code = ?", "D42") // find product with code D42
-
-	// Update - update product's price to 200
-	// db.Model(&product).Update("Price", 200)
-	// Update - update multiple fields
-	// db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	// db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-
-	// Delete - delete product
-	// db.Delete(&product, 1)
-	// Launch another browser with the same docker container.
-	// ll := launcher.MustNewManaged(rodUrl)
-
-	// You can set different flags for each browser.
-	// ll.Set("disable-sync").Delete("disable-sync")
-
-	// anotherBrowser := rod.New().Client(ll.MustClient()).MustConnect()
-
-	// fmt.Println(
-	// 	anotherBrowser.MustPage("https://go-rod.github.io").MustEval("() => document.title"),
-	// )
-
-	// utils.Pause()
 }
